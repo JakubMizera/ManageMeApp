4a020ca586fab35614c986c36cef4b77bff9d363
JakubMizera <kubamiz53@gmail.com>
2023-07-18 22:07:11 +0200

    homepage displays projects

--- app/src/app/homepage/homepage.component.ts
+++ app/src/app/homepage/homepage.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../types';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  projects!: Project[];
+
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.projects = this.projectService.getAllProjects();
+  };
 
 }
