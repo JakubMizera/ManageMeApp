910e524363aff7437f608dda818b7c31583454ad
JakubMizera <kubamiz53@gmail.com>
2023-07-24 13:30:20 +0200

    routing for functionality create

--- app/src/app/project-view/project-view.component.ts
+++ app/src/app/project-view/project-view.component.ts
@@ -31,7 +31,7 @@ export class ProjectViewComponent implements OnInit {
   };
 
   navigateToFunctionalityCreate(id: number): void {
-    this.router.navigate(['/project/:id/functionality/add', id])
-  }
+    this.router.navigate(['project', id, 'functionality', 'add']);
+  };
 
 };
