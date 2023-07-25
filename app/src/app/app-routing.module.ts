7bee3d59b8870e24de731b9208eb30ba19c3ed1f
JakubMizera <kubamiz53@gmail.com>
2023-07-25 09:34:39 +0200

    routing for functionality component

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,11 +5,18 @@ import { ProjectViewComponent } from './project-view/project-view.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { FunctionalityCreateComponent } from './functionality-create/functionality-create.component';
+import { FunctionalityViewComponent } from './functionality-view/functionality-view.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'project/add', component: ProjectCreateComponent },
-  { path: 'project/:id', component: ProjectViewComponent },
+  {
+    path: 'project/:id',
+    component: ProjectViewComponent,
+    children: [
+      { path: '', component: FunctionalityViewComponent }
+    ],
+  },
   { path: 'project/edit/:id', component: ProjectEditComponent },
   { path: 'project/:id/functionality/add', component: FunctionalityCreateComponent },
 ];
