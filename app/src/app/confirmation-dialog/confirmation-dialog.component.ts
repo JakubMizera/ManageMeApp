4ec9079f2a24725bcaa44ba0776e4fb45763cc47
JakubMizera <kubamiz53@gmail.com>
2023-07-25 12:44:08 +0200

    confirmation dialog logic

--- app/src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ app/src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ConfirmationDialogComponent {
 
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>
+  ) { }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  };
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  };
 }
